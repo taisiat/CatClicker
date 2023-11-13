1dc2134ae856de009e6580eb8444494615b1f0a2
taisiat <113080884+taisiat@users.noreply.github.com>
2023-11-13 13:12:10 -0800

    Add css and heart animation

--- src/components/cattile.js
+++ src/components/cattile.js
@@ -2,24 +2,37 @@ import { useEffect, useState } from "react";
 
 function CatTile({ cat }) {
   const [counter, setCounter] = useState({});
+  const [clicked, setClicked] = useState(false);
 
   function handleCounterClick() {
     setCounter((prev) => {
       return { ...prev, [cat.id]: prev[cat.id] + 1 || 1 };
     });
+    setClicked(true);
+    // console.log("clicked", clicked);
+    setTimeout(() => {
+      setClicked(false);
+    }, 500);
   }
+
+  useEffect(() => {
+    console.log(clicked);
+  }, [clicked]);
 
   return (
     <>
       {cat && (
         <>
           <h2>{cat.name} is a cat</h2>
-          <img
-            className="cat-pic"
-            src={cat.url}
-            alt="Picture of cute cat"
-            onClick={handleCounterClick}
-          />
+          <div className="cat-pic-container">
+            <img
+              className="cat-pic"
+              src={cat.url}
+              alt="Picture of cute cat"
+              onClick={handleCounterClick}
+            />
+            <div className={`heart ${clicked ? "clicked" : ""}`}>❤️</div>
+          </div>
           <p>Click pic to add to love counter for {cat.name}^</p>
           <h3>
             Love counter for {cat.name}: {counter[cat.id] || 0}

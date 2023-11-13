77389b1c52726688013f29267a5d79bfe1df04b4
taisiat <113080884+taisiat@users.noreply.github.com>
2023-11-13 12:09:41 -0800

    Add a dumb counter for clicks on all cats

--- src/components/cattile.js
+++ src/components/cattile.js
@@ -1,12 +1,23 @@
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 
 function CatTile({ cat }) {
+  const [counter, setCounter] = useState(0);
+
+  function handleCounterClick() {
+    setCounter((prev) => prev + 1);
+  }
+
   return (
     <>
       {cat && (
         <>
           <h2>{cat.name}</h2>
-          <img src={cat.url} alt="Picture of cute cat" />q
+          <img
+            src={cat.url}
+            alt="Picture of cute cat"
+            onClick={handleCounterClick}
+          />
+          <h3>Counter:{counter}</h3>
         </>
       )}
       {!cat && <p>Pick a cute cat</p>}

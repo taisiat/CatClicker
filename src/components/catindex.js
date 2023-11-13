333d96c0b0b5fe849ab18866d79a4440d098be1c
taisiat <113080884+taisiat@users.noreply.github.com>
2023-11-13 14:49:44 -0800

    Add counter of how long been on page

--- src/components/catindex.js
+++ src/components/catindex.js
@@ -4,6 +4,12 @@ import { useState, useEffect } from "react";
 function CatIndex() {
   const [cats, setCats] = useState([]);
   const [cat, setCat] = useState(null);
+  const [time, setTime] = useState(0);
+
+  useEffect(() => {
+    const intId = setInterval(() => setTime((prev) => prev + 1), 1000);
+    return () => clearInterval(intId);
+  }, []);
 
   useEffect(() => {
     fetch("http://localhost:5000/getCatsDB")
@@ -11,6 +17,15 @@ function CatIndex() {
       .then((data) => setCats(data))
       .catch((err) => console.log(err));
   }, []);
+
+  function timeFormatter() {
+    let min = Math.floor(time / 60);
+    let hrs = Math.floor(min / 60);
+    let secs = time - 60 * min - 60 * 60 * hrs;
+    return `${hrs < 10 ? "0" + String(hrs) : hrs}:${
+      min < 10 ? "0" + String(min) : min
+    }:${secs < 10 ? "0" + String(secs) : secs}`;
+  }
 
   return (
     <>
@@ -28,6 +43,7 @@ function CatIndex() {
           );
         })}
       <CatTile cat={cat} />
+      <p>It has been {timeFormatter()} since you've refreshed this page!</p>
     </>
   );
 }
